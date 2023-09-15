cb137c62a0d510bd02f5a8d37ccbb21b49f53174
1KitCat1 <denischool119@gmail.com>
2023-09-15 16:16:42 +0300

    Add constant parameters to Game Constants

--- GameConstants.sol
+++ GameConstants.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract GameConstants {
-    uint256 FIELD_SIZE = 36;
-    uint256 MAX_DICE_VALUE = 6;
+    uint256 constant FIELD_SIZE = 36;
+    uint256 constant MAX_DICE_VALUE = 6;
 }
