c667fe127d1490a76d0673906671370bfb06afaf
1KitCat1 <denischool119@gmail.com>
2023-09-15 13:53:59 +0300

    Add randomDiceValue in Generator

--- RandomGenerator.sol
+++ RandomGenerator.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract RandomNumberGenerator {
+import './GameConstants.sol';
+
+contract RandomGenerator is GameConstants {
     uint256 entropy;
 
     constructor() {
         entropy = uint256(keccak256(abi.encodePacked(
             blockhash(block.number),
             block.timestamp,
-            block.difficulty,
             msg.sender
         )));
     }
 
-    function getRandomNumber() external returns(uint256) {
+    function getRandomNumber() public returns(uint256) {
         entropy = uint256(keccak256(abi.encodePacked(
             entropy,
             blockhash(block.number),
@@ -21,4 +22,9 @@ contract RandomNumberGenerator {
         )));
         return entropy;
     }
+
+    function getRandomDiceValue() public returns(uint256) {
+        return 1 + getRandomNumber() % MAX_DICE_VALUE;
+    }
+
 }
