36a8ad64135b3a8c37f6e2854822e09dc84d03d3
1KitCat1 <denischool119@gmail.com>
2023-09-20 15:37:56 +0300

    Add chance card controller

--- GameConstants.sol
+++ GameConstants.sol
@@ -5,4 +5,11 @@ contract GameConstants {
     uint256 constant FIELD_SIZE = 36;
     uint256 constant MAX_DICE_VALUE = 6;
     uint256 constant UPGRADE_DELTA = 100;
+    uint256 constant MOVES_TO_FREE = 3;
+    uint256 constant CHANCE_ACTION_TYPE_SHIFT = 10;
+    uint256 constant CHANCE_ACTION_VALUE_SHIFT = 25;
+    uint256 constant CHANCE_MOVE_MIN = 2;
+    uint256 constant CHANCE_MOVE_MAX = 12;
+    uint256 constant CHANCE_PAY_MIN = 300;
+    uint256 constant CHANCE_PAY_MAX = 1000;      
 }
